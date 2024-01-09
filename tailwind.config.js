cde163090c0c8c8e47411f93a76f5e45668ff4ac
Adrian Pramana <adrian.astalina.pramana25@gmail.com>
2024-01-09 13:20:06 +0800

    Siap untuk di-publish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: {
-        enabled: true,
-        content: ['index.html',
-            "./node_modules/flowbite/**/*.js"
-        ],
-        darkMode: 'class',
-        theme: {
-            container: {
-                center: true,
-                padding: '16px',
+    content: ['index.html',
+        "./node_modules/flowbite/**/*.js"
+    ],
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                primary: '#03001C',
+                secondary: '#64748b',
+                dark: '#0f172a',
             },
-            extend: {
-                colors: {
-                    primary: '#03001C',
-                    secondary: '#64748b',
-                    dark: '#0f172a',
-                },
-                screens: {
-                    '2xl': '1320px',
-                },
+            screens: {
+                '2xl': '1320px',
             },
         },
     },
